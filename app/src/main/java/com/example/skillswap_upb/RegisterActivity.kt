8f78c015b534ac463af14ec2a91fefc758e5d4a6
BrandonAllan <allanbrandon34@gmail.com>
2023-06-14 07:28:27 -0400

    Firebase setup for authentication

--- app/src/main/java/com/example/skillswap_upb/RegisterActivity.kt
+++ app/src/main/java/com/example/skillswap_upb/RegisterActivity.kt
@@ -2,10 +2,18 @@ package com.example.skillswap_upb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.content.Intent
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val signUpText: TextView = findViewById(R.id.alredyHaveAccount)
+        signUpText.setOnClickListener {
+            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
