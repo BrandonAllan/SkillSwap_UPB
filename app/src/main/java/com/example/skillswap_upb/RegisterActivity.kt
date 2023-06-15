b5c5fb98e0810d43ca1fe2f6c01c38252f80ab32
BrandonAllan <allanbrandon34@gmail.com>
2023-06-15 08:49:36 -0400

    Archivos actualizados

--- app/src/main/java/com/example/skillswap_upb/RegisterActivity.kt
+++ app/src/main/java/com/example/skillswap_upb/RegisterActivity.kt
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.content.Intent
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+    lateinit var signupEmail: EditText
+    lateinit var signupPassword: EditText
+    lateinit var signupUsername: EditText
+    lateinit var signupButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        auth = FirebaseAuth.getInstance();
+        signupEmail = findViewById(R.id.InputEmail)
+        signupPassword = findViewById(R.id.InputPassword)
+        signupUsername = findViewById(R.id.InputUsername)
+        signupButton = findViewById(R.id.buttonRegister)
 
         val signUpText: TextView = findViewById(R.id.alredyHaveAccount)
         signUpText.setOnClickListener {
