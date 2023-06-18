77ea4a740c24bfb70a755181c9223a48ce0dc271
BrandonAllan <allanbrandon34@gmail.com>
2023-06-18 14:58:08 -0400

    Firebase implementado

--- app/src/main/java/com/example/skillswap_upb/LoginActivity.kt
+++ app/src/main/java/com/example/skillswap_upb/LoginActivity.kt
@@ -4,17 +4,51 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.content.Intent
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 
 class LoginActivity : AppCompatActivity() {
+    //Firebase initialization
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        auth = FirebaseAuth.getInstance();
+
+        //Login button logic
+        val loginButton = findViewById<Button>(R.id.buttonLogin)
+        loginButton.setOnClickListener {
+            val email = findViewById<EditText>(R.id.InputEmail).text.toString().trim()
+            val password = findViewById<EditText>(R.id.InputPassword).text.toString().trim()
+            loginUser(email, password)
+        }
+
+        //Going to RegisterActivity if user doesn't have an account
         val signUpText: TextView = findViewById(R.id.SignUpText)
         signUpText.setOnClickListener {
-            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
     }
+
+    private fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Login successful, proceed to the next activity
+                    Toast.makeText(this, "Login successful!", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // Login failed, display an error message
+                    Toast.makeText(this, "Authentication failed. Please try again.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
