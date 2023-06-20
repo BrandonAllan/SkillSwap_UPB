37d3f04b0ff4eaae5b09468fcbe956beb39072f2
Resh3l <134703979+Resh3l@users.noreply.github.com>
2023-06-19 21:36:13 -0400

    Update MainActivity.kt
    Modificaciones de historial

--- app/src/main/java/com/example/skillswap_upb/MainActivity.kt
+++ app/src/main/java/com/example/skillswap_upb/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.skillswap_upb
 
 import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.proyecto.R.id.historial
+
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+     val buton=findViewById<Button>(R.id.historial)
+        buton.setOnClickListener{
+            val lanzar= Intent(this,historalChats::class.java)
+            startActivity(lanzar)
+        }
     }
+
+
 }
