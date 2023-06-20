c95211260ebe77f8100bb692a99a7875a768e51c
Resh3l <134703979+Resh3l@users.noreply.github.com>
2023-06-19 21:34:15 -0400

    Create historialChats.kt
    Historial de chats

---
+++ app/src/main/java/com/example/skillswap_upb/historialChats.kt
@@ -0,0 +1,11 @@
+package com.example.proyecto
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class historalChats : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_historal_chats)
+    }
+}
