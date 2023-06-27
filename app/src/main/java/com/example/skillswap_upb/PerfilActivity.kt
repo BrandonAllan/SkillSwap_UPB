6840be8b36c881abf312c59dec7c25c71e59bbc6
Dilanvelasquez <dilandk945@gmail.com>
2023-06-27 12:43:11 -0400

    Logic botton Navegation

--- app/src/main/java/com/example/skillswap_upb/PerfilActivity.kt
+++ app/src/main/java/com/example/skillswap_upb/PerfilActivity.kt
@@ -1,10 +1,14 @@
 package com.example.skillswap_upb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -19,8 +23,34 @@ class PerfilActivity : AppCompatActivity() {
         val deleteAccountButton = findViewById<Button>(R.id.Deleteperfil)
         deleteAccountButton.setOnClickListener {
             deleteAccount()
+        }
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_nav -> {
+                    loadActivity(MainActivity::class.java)
+                    true
+                }
+                R.id.search_nav -> {
+                    loadActivity(ApredizajeActivity::class.java)
+                    true
+                }
+                R.id.perfil_nav -> {
+                    loadActivity(PerfilActivity::class.java)
+                    true
+                }
+                else -> false
             }
         }
+    }
+
+    private fun loadActivity(activityClass: Class<out AppCompatActivity>) {
+        val intent = Intent(this, activityClass)
+        startActivity(intent)
+        finish()
+    }
+
     private fun deleteAccount() {
         val user = auth.currentUser
         user?.delete()
