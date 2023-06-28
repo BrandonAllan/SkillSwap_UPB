89c656731a648e3308a291ddee1d68cfad951819
CesarApaza <apaza.cesar.1812@gmail.com>
2023-06-28 03:10:29 -0400

    Pantalla lista creado

--- app/src/main/java/com/example/skillswap_upb/MainActivity.kt
+++ app/src/main/java/com/example/skillswap_upb/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.skillswap_upb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.buttoncambio)
+        button.setOnClickListener { val intent = Intent(this, ApredizajeActivity::class.java) }
+        startActivity(intent)
+
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_nav -> {
+                    loadActivity(ChatList::class.java)
+                    true
+                }
+                R.id.search_nav -> {
+                    loadActivity(MainActivity::class.java)
+                    true
+                }
+                R.id.perfil_nav -> {
+                    loadActivity(PerfilActivity::class.java)
+                    true
+                }
+                else -> false
+            }
+        }
     }
-}
+
+    private fun loadActivity(activityClass: Class<out AppCompatActivity>) {
+        val intent = Intent(this, activityClass)
+        startActivity(intent)
+        finish()
+    }
+
+
+    }
